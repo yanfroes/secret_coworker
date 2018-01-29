04143308609fc75beab862f8d0f7e91edf7ce144
yanz <yanfroes@gmail.com>
2018-01-29 16:57:01 -0200

    Set Controller Member

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,16 +1,19 @@
 class Campaign < ApplicationRecord
+  after_validation :set_member, on: :create
+  before_validation :set_status, on: :create
+
   belongs_to :user
   has_many :members, dependent: :destroy
-  before_create :set_status
-  before_create :set_member
   enum status: [:pending, :finished]
   validates :title, :description, :user, :status, presence: true
+
+  private
 
   def set_status
     self.status = :pending
   end
 
   def set_member
-    self.members << Member.create(area: self.user.area, name: self.user.name, email: self.user.email)
+    self.members << Member.create(name: self.user.name, email: self.user.email)
   end
 end
