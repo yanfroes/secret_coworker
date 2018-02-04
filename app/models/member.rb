3530a01986fabc7a8ede6562ea7bdb29e3e6b9a7
Unknown <yanfroes@gmail.com>
2018-02-04 20:01:58 -0200

    Criação do Coffeescript (member)

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,6 +1,6 @@
 class Member < ApplicationRecord
   belongs_to :campaign
-  after_save :set_campaign_pending
+  after_destroy :set_campaign_pending
   validates :area, :name, :email, :campaign, presence: true
 
   def set_pixel
